c4405bd78b4f62b32f99fd5d38fafa4341189dab
Richie Fulop <richard.fulop@intalere.com>
2020-01-31 16:35:40 -0500

    Initial commit of app server

---
+++ app/app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+const session = require('express-session')
+const uuid = require('uuid/v4')
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(process.env.TDM_AUTH_SECRET);
+app.use(session({
+  genid: (req) => {
+    return uuid()
+  },
+  secret: process.env.TDM_AUTH_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/img', express.static(__dirname + '/public/img')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
