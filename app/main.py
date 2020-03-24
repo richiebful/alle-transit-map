e672f4c0c8feb840332fb54c176df08a0d96d33b
Richie Fulop <richard.fulop@intalere.com>
2020-03-24 19:08:29 -0400

    Change templates from handlebars to jinja

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 import flask
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path='')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/img/<path>')
+def serve_images(path: str):
+    return flask.send_from_directory('public/img', path)
+
+@app.route('/stylesheets/<path>')
+def serve_css(path: str):
+    return flask.send_from_directory('public/css', path)
